b5e5df219951b98390b2725f9f47ceee93c0a5c3
jeroen <info@jeroenvandenhoek.nl>
2023-06-28 22:19:17 +0200

    changed behavior of tab, to work for copilot

--- lua/astronvim/lazy.lua
+++ lua/astronvim/lazy.lua
@@ -62,10 +62,10 @@ require('copilot').setup({
   },
   suggestion = {
     enabled = true,
-    auto_trigger = false,
+    auto_trigger = true,
     debounce = 75,
     keymap = {
-      accept = "<M-l>",
+      accept = "<Tab>",
       accept_word = false,
       accept_line = false,
       next = "<M-]>",
