d69ed23a015a45bfd40640884927f54044bd8315
jeroenvandenhoek <jeroen.vandenhoek@iodigital.com>
2023-08-04 08:25:50 +0200

    fix typing

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
+
+local powershell_options = {
+  shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shellquote = "",
+  shellxquote = "",
+}
+
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
+
 for _, source in ipairs {
   "astronvim.bootstrap",
   "astronvim.options",
@@ -24,4 +38,3 @@ vim.o.background = "dark"
 vim.cmd [[colorscheme gruvbox]]
 
 vim.cmd([[au BufReadPost *.hbs set syntax=html]])
-
